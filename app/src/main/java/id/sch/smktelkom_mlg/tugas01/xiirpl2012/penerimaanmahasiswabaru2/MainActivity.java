2bf0490e32c79351c646fddaa29c571f6fa49cc7
Dita Ayu <ditaayu0609@gmail.com>
2016-10-10 08:51:47 +0700

    menampilkan output checkbox dan radiobutton

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl2012/penerimaanmahasiswabaru2/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl2012/penerimaanmahasiswabaru2/MainActivity.java
@@ -3,12 +3,17 @@ package id.sch.smktelkom_mlg.tugas01.xiirpl2012.penerimaanmahasiswabaru2;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     EditText nama, telp;
     TextView hasil;
+    RadioGroup jk;
+    CheckBox SI, TP, KB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,10 @@ public class MainActivity extends AppCompatActivity {
         nama = (EditText) findViewById(R.id.editTextNama);
         telp = (EditText) findViewById(R.id.editTextNoTelp);
         hasil = (TextView) findViewById(R.id.textViewHasil);
+        jk = (RadioGroup) findViewById(R.id.RadioGroupJK);
+        SI = (CheckBox) findViewById(R.id.checkBoxSI);
+        TP = (CheckBox) findViewById(R.id.checkBoxTP);
+        KB = (CheckBox) findViewById(R.id.checkBoxKB);
         findViewById(R.id.buttonSAVE).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,9 +36,32 @@ public class MainActivity extends AppCompatActivity {
 
     private void doClick() {
         if (isValid()) {
-            String nm = nama.getText().toString();
-            int nmr = Integer.parseInt(telp.getText().toString());
-            hasil.setText("Nama : " + nm + "\n" + "Nomer Telp : " + nmr + "\n");
+            String hsl = null;
+            String hl = "Jurusan Anda:\n";
+            int startlen = hl.length();
+            if (SI.isChecked()) hl += SI.getText() + "\n";
+            if (TP.isChecked()) hl += TP.getText() + "\n";
+            if (KB.isChecked()) hl += KB.getText() + "\n";
+
+            if (hl.length() == startlen) hl += "Tidak Ada Pilihan";
+
+            if (jk.getCheckedRadioButtonId() != -1) {
+                RadioButton rb = (RadioButton)
+                        findViewById(jk.getCheckedRadioButtonId());
+                hsl = rb.getText().toString();
+            }
+            if (hsl == null) {
+                hasil.setText("Belum memilih jenis kelamin");
+            } else {
+                String nm = nama.getText().toString();
+                int nmr = Integer.parseInt(telp.getText().toString());
+                StringBuilder builder = new StringBuilder();
+                builder.append("Nama : " + nm + "\n");
+                builder.append("Nomor Telp : " + nmr + "\n");
+                builder.append("Jenis Kelamin anda :" + hsl + "\n");
+                builder.append(hl);
+                hasil.setText(builder);
+            }
         }
 
     }
